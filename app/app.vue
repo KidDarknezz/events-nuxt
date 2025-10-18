f9d7b783eb7f336bd9aa8fea7964d7bb00fe5ad5
amillanbc <amillan@bluecorela.com>
2025-10-18 14:38:47 -0500

    Fix: project structure refactor

--- app/app.vue
+++ app/app.vue
@@ -1,78 +1,119 @@
-<script setup>
-useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
-  htmlAttrs: {
-    lang: 'en'
-  }
-})
-
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
-
-useSeoMeta({
-  title,
-  description,
-  ogTitle: title,
-  ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterCard: 'summary_large_image'
-})
-</script>
-
 <template>
-  <UApp>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtLoadingIndicator />
     <UHeader>
-      <template #left>
-        <NuxtLink to="/">
-          <AppLogo class="w-auto h-6 shrink-0" />
-        </NuxtLink>
-
-        <TemplateMenu />
+      <template #title>
+        <OSLogo class="h-8 w-auto" />
       </template>
-
       <template #right>
-        <UColorModeButton />
+        <!-- <UColorModeButton /> -->
 
-        <UButton
-          to="https://github.com/nuxt-ui-templates/starter"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-          color="neutral"
-          variant="ghost"
-        />
+        <UPopover arrow>
+          <UButton
+            label="Iniciar sesión"
+            variant="ghost"
+            color="neutral"
+            icon="i-lucide-user"
+          />
+          <template #content>
+            <div class="px-4 py-4 flex flex-col w-60">
+              <UInput
+                color="neutral"
+                variant="soft"
+                placeholder="Correo electrónico"
+                class="mb-2"
+                size="lg"
+              />
+              <UInput
+                color="neutral"
+                variant="soft"
+                placeholder="Contraseña"
+                class="mb-3"
+                size="lg"
+              />
+              <UButton
+                class="bg-rose-500 hover:bg-rose-600 active:bg-rose-400 text-white mb-1"
+              >
+                <div class="w-full">Iniciar sesión</div>
+              </UButton>
+              <UButton color="neutral" variant="subtle" class="mb-4">
+                <div class="flex items-center justify-center w-full gap-2">
+                  <img :src="Google" width="15px" />
+                  <div>Google</div>
+                </div>
+              </UButton>
+              <USeparator />
+              <UButton
+                class="mt-4 text-blue-500 hover:text-blue-600 active:text-blue-400 mb-3"
+                variant="link"
+                size="xs"
+              >
+                <div class="w-full text-center">Regístrate</div>
+              </UButton>
+              <div class="text-xs text-center">
+                <span class="text-rose-500 underline"
+                  >Términos y condiciones</span
+                >
+                para registro de usuario
+              </div>
+            </div>
+          </template>
+        </UPopover>
       </template>
     </UHeader>
+    <div class="bg-blue-500 py-4 text-white fixed w-full">
+      <UContainer>
+        <div class="flex justify-between items-center">
+          <div>Instala nuestra app.</div>
+          <div class="flex gap-2">
+            <img :src="AppStoreBadge" width="100px" />
+            <img :src="PlayStoreBadge" width="100px" />
+          </div>
+        </div>
+      </UContainer>
+    </div>
+    <div class="bg-warning-300 mt-[63px] text-black pt-15">
+      <UContainer class="h-full flex items-end">
+        <div class="flex flex-row w-full items-center">
+          <div class="flex-1">
+            <h1 class="text-4xl font-bold mb-6">
+              La tecnología para eventos más avanzada de Panamá
+            </h1>
+            <h2 class="text-base mb-6">
+              Publica gratis tus eventos sin importar tu boletería de elección.
+            </h2>
+            <div class="flex gap-2">
+              <img :src="AppStoreBadge" width="150px" />
+              <img :src="PlayStoreBadge" width="150px" />
+            </div>
+          </div>
+          <div class="flex-1">
+            <img :src="Hero" class="h-165 w-auto mx-auto" />
+          </div>
+        </div>
+      </UContainer>
+    </div>
 
-    <UMain>
-      <NuxtPage />
-    </UMain>
-
-    <USeparator icon="i-simple-icons-nuxtdotjs" />
-
-    <UFooter>
-      <template #left>
-        <p class="text-sm text-muted">
-          Built with Nuxt UI • © {{ new Date().getFullYear() }}
-        </p>
-      </template>
-
-      <template #right>
-        <UButton
-          to="https://github.com/nuxt-ui-templates/starter"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-          color="neutral"
-          variant="ghost"
-        />
-      </template>
-    </UFooter>
-  </UApp>
+    <!-- <UContainer>
+      <div class="min-h-screen grid place-items-center text-center">
+        <div class="grid grid-cols-12 gap-4 w-full">
+          <UCard class="lg:col-span-2 md:col-span-6 col-span-12">One</UCard>
+          <UCard class="lg:col-span-2 md:col-span-6 col-span-12">Two</UCard>
+          <UCard class="lg:col-span-2 md:col-span-6 col-span-12">Three</UCard>
+          <UCard class="lg:col-span-2 md:col-span-6 col-span-12">Four</UCard>
+          <UCard class="lg:col-span-2 md:col-span-6 col-span-12">Five</UCard>
+          <UCard class="lg:col-span-2 md:col-span-6 col-span-12">Six</UCard>
+        </div>
+      </div>
+    </UContainer> -->
+  </div>
 </template>
+
+<script setup lang="ts">
+// import OSLogo from "./components/OSLogo.vue";
+import AppStoreBadge from "@/assets/images/app-store-badge.svg";
+import PlayStoreBadge from "@/assets/images/play-store-badge.svg";
+import Hero from "@/assets/images/hero.png";
+import Google from "@/assets/images/google.svg";
+</script>
---
+++ app/components/OSLogo.vue
@@ -0,0 +1,7 @@
+<template>
+  <img :src="OSLogo" alt="One Spot Logo" />
+</template>
+
+<script lang="ts" setup>
+import OSLogo from "@/assets/images/os-logo-full.png";
+</script>
